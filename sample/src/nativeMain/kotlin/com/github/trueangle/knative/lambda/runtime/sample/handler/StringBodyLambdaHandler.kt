34a37d2334dbf7caa020de9b033aa0399da9ab47
Viacheslav Ivanovichev <s.ivanovichev@gmail.com>
2024-08-25 12:15:09 +0200

    Add additional logs

--- sample/src/nativeMain/kotlin/com/github/trueangle/knative/lambda/runtime/sample/handler/StringBodyLambdaHandler.kt
+++ sample/src/nativeMain/kotlin/com/github/trueangle/knative/lambda/runtime/sample/handler/StringBodyLambdaHandler.kt
@@ -6,8 +6,6 @@ import io.github.trueangle.knative.lambda.runtime.handler.LambdaHandler
 
 class StringBodyLambdaHandler : LambdaBufferedHandler<String, String> {
     override suspend fun handleRequest(input: String, context: Context): String {
-        println("Invoke lambda handler\n payload: $input\n context: $context")
-
         return """ {
                   "statusCode": 200,
                   "headers": {
