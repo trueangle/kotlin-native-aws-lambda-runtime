961a60f0202356d41815136ba19793c72de93d14
Viacheslav Ivanovichev <s.ivanovichev@gmail.com>
2024-09-24 14:34:35 +0200

    Rollback to std memory allocator as the fastest one

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -14,8 +14,7 @@ kotlin {
         it.binaries {
             executable {
                 entryPoint = "com.github.trueangle.knative.lambda.runtime.sample.main"
-                //freeCompilerArgs += listOf("-Xallocator=std")
-                freeCompilerArgs += listOf("-Xallocator=mimalloc")
+                freeCompilerArgs += listOf("-Xallocator=std")
             }
         }
     }
