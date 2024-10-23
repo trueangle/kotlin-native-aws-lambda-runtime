5e7a16601d16c84d1426cd320bf7e61535727c33
Viacheslav Ivanovichev <s.ivanovichev@gmail.com>
2024-10-23 14:31:29 +0200

    Update build.gradle.kts

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -14,7 +14,7 @@ kotlin {
         it.binaries {
             executable {
                 entryPoint = "com.github.trueangle.knative.lambda.runtime.sample.main"
-                freeCompilerArgs += listOf("-Xallocator=mimalloc")
+                // freeCompilerArgs += listOf("-Xallocator=mimalloc")
             }
         }
     }
