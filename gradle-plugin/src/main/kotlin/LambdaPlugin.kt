5654cc93986c4796ccb2048dcc9a7948d47638f5
Daniel Eichman <61132910+zfz7@users.noreply.github.com>
2025-03-07 08:25:21 -0800

    Enabled ARM support  (#8)
    Enabled ARM support

--- gradle-plugin/src/main/kotlin/LambdaPlugin.kt
+++ gradle-plugin/src/main/kotlin/LambdaPlugin.kt
@@ -1,16 +1,55 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.property
 import java.io.File
 
 class LambdaPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.tasks.create("buildLambdaRelease", LambdaPackagerTask::class.java)
+        project.run {
+            val buildLambdaRelease = extensions.create(
+                "buildLambdaRelease",
+                LambdaPackagerExtension::class.java,
+                project
+            )
+            tasks.apply {
+                register("buildLambdaRelease", LambdaPackagerTask::class.java).configure {
+                    architecture.set(buildLambdaRelease.architecture)
+                }
+            }
+        }
+    }
+}
+
+enum class Architecture(val path: String) {
+    LINUX_X64("linuxX64"),
+    LINUX_ARM64("linuxArm64");
+
+    override fun toString(): String = path
+}
+
+/**
+ * Gradle project level extension object definition for the LambdaPackagerTask
+ *
+ */
+open class LambdaPackagerExtension(project: Project) {
+    /**
+     * The architecture of the desired Lambda runtime, default: LINUX_X64
+     */
+    val architecture = project.objects.property<Architecture>()
+
+    init {
+        architecture.set(Architecture.LINUX_X64)
     }
 }
 
 abstract class LambdaPackagerTask : DefaultTask() {
+    @Input
+    val architecture: Property<Architecture> =
+        project.objects.property(Architecture::class.java).convention(Architecture.LINUX_X64)
 
     @TaskAction
     fun packageLambda() {
@@ -20,7 +59,7 @@ abstract class LambdaPackagerTask : DefaultTask() {
         }
 
         val buildDir = project.layout.buildDirectory
-        val executableDir = project.file(buildDir.dir("bin/linuxX64/releaseExecutable"))
+        val executableDir = project.file(buildDir.dir("bin/${architecture.get()}/releaseExecutable"))
         val executable = executableDir
             .listFiles()
             ?.first { it.name.endsWith(".kexe") }
